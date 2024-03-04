e9a2ecade9b2220dd0f0e626fa414874f6f317c6
Saurab Prasher <saurabprasher@saurabs-mbp.home>
2024-03-03 20:22:57 -0500

    hero section styles added

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,37 +1,47 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div>
-      {/* Hero Section */}
-      <section className='text-center py-24 px-4'>
-        <div className='max-w-screen-xl mx-auto'>
-          <div className='mb-8'>
-            <h1 className='text-4xl font-bold mb-4'>
-              A healthy meal delivered to your door, every single day
-            </h1>
-            <p className='mb-8'>
-              The smart 365-days-per-year food subscription that will make you
-              eat healthy again. Tailored to your personal tastes and
-              nutritional needs. We have delivered 250,000+ meals last year!
-            </p>
-            <a
-              href='login'
-              className='bg-orange-500 text-white py-2 px-4 rounded-md mr-2'
-            >
-              Start eating well
-            </a>
-            <a
-              href='#how'
-              className='bg-transparent border-2 border-orange-500 hover:bg-orange-500 hover:text-white text-orange-500 py-2 px-4 rounded-md transition duration-150 ease-in-out'
-            >
-              Learn more ↓
-            </a>
-          </div>
-          <div className='flex justify-center'></div>
+    <section className='relative h-max '>
+      <div className='absolute top-0 left-0 w-full h-full'>
+        <Image
+          src='/images/heroSection.jpg'
+          alt='Healthy Food'
+          layout='fill'
+          objectFit='cover'
+          quality={100}
+        />
+
+        <div className='absolute top-0 left-0 w-full h-full bg-black opacity-60'></div>
+      </div>
+
+      <div className=' mx-auto px-6 pt-32 pb-56 text-white z-20 relative'>
+        <div className='w-full md:w-1/2 m-auto'>
+          <h2 className='text-6xl  font-light tracking-wide'>
+            Healthy Meals Delivered to Your Door
+          </h2>
+          <p className='text-2xl mt-4  text-white font-light tracking-wide'>
+            Discover your perfect meal plan. Tailored to your personal tastes
+            and nutritional needs.
+          </p>
+
+          <Link
+            className='mt-4 bg-[#FFA726] text-base text-[#f4f4f4] px-4 py-2  inline-block rounded hover:bg-[#d19844] transition duration-200'
+            href='/login'
+          >
+            Get Started
+          </Link>
+          <Link
+            className='mt-4 ml-6 bg-[#4CAF50] text-base text-white px-4 py-2  inline-block rounded hover:bg-[#2f8f32] transition duration-200'
+            href='/login'
+          >
+            Know more
+          </Link>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
