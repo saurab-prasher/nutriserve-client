f5a8ecc562f73b9448a54bf03417a6fc0ec18941
chandrika <chandrikakvenu@gmail.com>
2024-03-08 22:29:21 -0500

    wishlist method

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,8 +2,6 @@
 import { useState, useContext, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { MyContext } from "../context/Context";
 
 export default function Navbar() {
@@ -11,6 +9,10 @@ export default function Navbar() {
   const { loggedInUser, handleLogout, selectedRecipes, likedRecipes } =
     useContext(MyContext);
   const [recipeCount, setRecipeCount] = useState(0);
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  console.log(isHovered);
 
   useEffect(() => {
     setRecipeCount(selectedRecipes?.length);
@@ -20,7 +22,7 @@ export default function Navbar() {
 
   return (
     <header>
-      <div className="mx-auto flex items-center justify-between px-12">
+      <div className="mx-auto flex items-center justify-between px-20">
         <Link href="/">
           <Image
             src="/images/logo/logo-nutriserve.png"
@@ -102,20 +104,15 @@ export default function Navbar() {
                 </span>
               </Link>
             </div>
-            <li>
+            {/* <li>
               <Link
-                className="text-gray-700 hover:text-gray-900 transition duration-150 ease-in-out"
-                href="/wishlist"
+                className='text-gray-700 hover:text-gray-900 transition duration-150 ease-in-out'
+                href='/search'
               >
-                <div className="flex items-center justify-between ">
-                  Wishlist
-                  <button className="text-red-500 focus:outline-none pl-2">
-                    <FontAwesomeIcon icon={faHeart} />
-                  </button>
-                </div>
+                Search
               </Link>
-            </li>
-            <li>
+            </li> */}
+            <li className="relative">
               {!loggedInUser ? (
                 <Link
                   className="mx-auto cursor-pointer text-center font-bold border m-auto py-2 px-4 text-md text-custom-green border-custom-green rounded-sm mb-4 hover:bg-custom-green hover:text-white"
@@ -124,23 +121,170 @@ export default function Navbar() {
                   Sign In
                 </Link>
               ) : (
-                <div className="flex items-center space-x-3">
-                  <Image
-                    src={loggedInUser?.avatar || "/images/avatar-img.webp"} // Replace "/
-                    alt="User Avatar"
-                    className="h-8 w-8 rounded-full object-cover" // Adjust the size as needed
-                    width={60}
-                    height={60}
-                  />
-                  <span className="font-bold">{loggedInUser?.firstname}</span>
-                  <button
-                    onClick={handleLogout}
-                    className="cursor-pointer text-center font-bold py-2 px-4 text-md text-custom-green border border-custom-green rounded-sm
-                    hover:bg-custom-green hover:text-white
-                    "
+                <div
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                  className="flex gap-2 cursor-pointer"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
                   >
-                    Log Out
-                  </button>
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                    />
+                  </svg>
+
+                  <div className=" z-100 ">
+                    <div className="flex items-end gap-2  ">
+                      <span className="capitalize">
+                        {loggedInUser?.firstname}
+                      </span>
+                      {isHovered ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                          />
+                        </svg>
+                      )}
+                    </div>
+                    {isHovered && (
+                      <div className="absolute top-4 -left-16 w-60 h-fit mt-2 p-5 shadow-xl rounded-md bg-white z-50  flex flex-col gap-5">
+                        <div className="flex gap-4 mt-6 hover:text-custom-green">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
+                            />
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                            />
+                          </svg>
+                          <p>Account Settings</p>
+                        </div>
+
+                        <div className="flex gap-4 hover:text-custom-green">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                            />
+                          </svg>
+
+                          <p>Favorite Recipes</p>
+                        </div>
+
+                        <div className="flex gap-4 hover:text-custom-green">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                            />
+                          </svg>
+
+                          <p>Favorite Recipes</p>
+                        </div>
+
+                        <div className="flex gap-4 hover:text-custom-green">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                            />
+                          </svg>
+
+                          <p>Favorite Recipes</p>
+                        </div>
+
+                        <div className="flex gap-4 border-t-[1px] pt-6">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+                            />
+                          </svg>
+                          <button
+                            onClick={handleLogout}
+                            className="cursor-pointer"
+                          >
+                            Log Out
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
               )}
             </li>
